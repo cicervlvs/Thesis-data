36fedb8937612a02f81fd3a222971fc84c892479
Alejandro <79423618+cicervlvs@users.noreply.github.com>
2022-04-01 19:15:22 +0200

    Fixed toneSimple dataframe that must've gotten messed up in the last change; added some dataframes and tables to distinguish between simple and complex tone in SA languages

--- SA_stats/wals-tone/wals-tone-SA.R
+++ SA_stats/wals-tone/wals-tone-SA.R
@@ -13,10 +13,20 @@ toneValues <- read.delim("tone.txt",
 toneSimple <- toneValues %>% mutate(hasTone =
                            if_else(.$description == "Complex tone system" |
                                    .$description == "Simple tone system",
-                                     'tone'))
+                                     'tone',
+                                    'no tone'))
 
 tone_SA <- toneSimple %>%
           filter(.$macroarea == "South America")
+
+tabSimpCompTone <- table(toneValues$description)
+
+simpCompToneSA <- toneValues %>% filter(.$macroarea == "South America") 
+tabSimCompSA <- table(simpCompToneSA$description)
+tabSimCompSAOnlyTone <- table(simpCompToneSA$description,
+                              exclude = "No tones")
+
+
 
 toneFreqTab <- table(tone_SA$hasTone)
 tonePropTab <- prop.table(freqTab)
